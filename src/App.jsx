48765ab50c1529fee1fe51141103548f2be1e9b6
Macarena Florencio <florenciomaca2@gmail.com>
2026-06-25 19:23:54 -0300

    Agrega componente Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import "./App.css"
+import Banner from "./components/Banner"
 import Navbar from "./components/Navbar"
 import ItemListContainer from "./components/ItemListContainer"
 
 function App() {
   return (
     <>
+      <Banner />
       <Navbar />
       <ItemListContainer greeting={"Bienvenido a Las Florencio"}/>
     </>
---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+function Banner () {
+    return (
+        <div className= "banner">
+            <p className= "banner-texto">
+                 • RENOVAMOS SHOWROOM - TODO EN SALE - HASTA 50% OFF •
+            </p>
+        </div>
+    )
+}
+
+export default Banner
