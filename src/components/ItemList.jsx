76e86504e41a0ddc72a5fd62f921f493ed5f963a
Macarena Florencio <florenciomaca2@gmail.com>
2026-06-30 18:56:33 -0300

    Agrega listado dinámico de productos con map y mejora estilos

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -20,29 +20,113 @@ import vdoNegrob from "../assets/imagen/VestidoCuelloNegroBack.jpg"
 import vdoHombreras from "../assets/imagen/VestidoconHombreras.jpg"
 import vdoRealf from "../assets/imagen/VestidoReal.jpg"
 
+const productos = [
+    {
+        id: 1,
+        nombre: "Remera Básica",
+        precio: 25000,
+        imagen: remeraBasica,
+    },
+    {
+        id: 2,
+        nombre: "Remera Channel",
+        precio: 23000,
+        imagen: remeraChannel,
+    },
+    {
+        id: 3,
+        nombre: "Remera Moschino",
+        precio: 24000,
+        imagen: remeraMoschino,
+    },
+    {
+        id: 4,
+        nombre: "Palazzo Cleo",
+        precio: 35000,
+        imagen: palazzoTiras,
+    },
+    {
+        id: 5,
+        nombre: "Jogger Cameron",
+        precio: 36000,
+        imagen: joggerCameron,
+    },
+    {
+        id: 6,
+        nombre: "Babucha Randall",
+        precio: 33000,
+        imagen: babuchaEcocuero,
+    },
+    {
+        id: 7,
+        nombre: "Poleron Julie",
+        precio: 28000,
+        imagen: poleron,
+    },
+    {
+        id: 8,
+        nombre: "Sweater Tabata",
+        precio: 21000,
+        imagen: sweaterCuadrille,
+    },
+    {
+        id: 9,
+        nombre: "Sweater Star",
+        precio: 29000,
+        imagen: sweaterEstrella,
+    },
+    {
+        id: 10,
+        nombre: "Sweater Ellis",
+        precio: 29000,
+        imagen: sweaterOchos,
+    },
+    {
+        id: 11,
+        nombre: "Sweater Boyd",
+        precio: 29000,
+        imagen: sweaterRayado,
+    },
+    {
+        id: 12,
+        nombre: "Vestido Shine",
+        precio: 40000,
+        imagen: vdoBrillos,
+    },
+    {
+        id: 13,
+        nombre: "Vestido Victoria",
+        precio: 30000,
+        imagen: vdoHombreras,
+    },
+    {
+        id: 14,
+        nombre: "Vestido Celine",
+        precio: 38000,
+        imagen: vdoNegrof,
+    },
+    {
+        id: 15,
+        nombre: "Vestido Real",
+        precio: 41000,
+        imagen: vdoRealf,
+    }
+]
+
 function ItemList() {
     return (
         <>
             <h2>Productos</h2>
             <div className="item-list">
-                <Item 
-                nombre="Remera Básica"
-                precio={25000}
-                imagen={remeraBasica}
-                />
-
-                <Item 
-                       nombre="Palazzo Cleo"
-                precio={35000}
-                imagen={palazzoTiras}
-                />
-                <Item 
-                       nombre="Vestido Real"
-                precio={40000}
-                imagen={vdoRealf}
-                imagen={vdoRealb}
-                />
-
+                {
+                    productos.map((producto) => (
+                        <Item
+                            nombre={producto.nombre}
+                            precio={producto.precio}
+                            imagen={producto.imagen}
+                        />
+                    ))
+                }
             </div>
         </>
     );
