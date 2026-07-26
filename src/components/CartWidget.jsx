8251b213a6cc8d3b28b3f1fe17668badd728a572
Macarena Florencio <florenciomaca2@gmail.com>
2026-07-26 19:35:13 -0300

    Implementa carrito con Context API

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
 import "../components/CartWidget.css";
 
 function CartWidget () {
+
+    const { carrito } = useContext(CartContext);
+
     return (
         <div className="cart-widget">
             <i className="bi bi-bag"></i>
-            <span>0</span>
+            <span>{carrito.length}</span>
         </div>
     );
 }
